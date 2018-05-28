c14c71f1d9396818a73154f79b067ab2cb9c7710
NoelleDL <noelle.legrain@gmail.com>
2018-05-28 19:53:25 +0100

    added method to play sound file

--- rocketapp/ViewController.swift
+++ rocketapp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -17,9 +18,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var hustleLabel: UILabel!
     @IBOutlet weak var onLabel: UILabel!
     
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+        player = try AVAudioPlayer(contentsOf: url)
+        player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
     
     @IBAction func powerButtonPressed(_ sender: Any) {
